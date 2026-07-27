193c0a9805300b382f9c55139643e19bd232f5cb
charantej64 <charantejaanem38@gmail.com>
2026-07-27 09:14:10 +0530

    Update (1232) Check if it is a Straight Line.java

--- (1232) Check if it is a Straight Line.java
+++ (1232) Check if it is a Straight Line.java
@@ -1,18 +1,20 @@
-class Solution {
+Class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-
-        int deltaX = coordinates[1][0] - coordinates[0][0];
-        int deltaY = coordinates[1][1] - coordinates[0][1];
         
+        int x0 = coordinates[0][0], y0 = coordinates[0][1];
+        int x1 = coordinates[1][0], y1 = coordinates[1][1];
+
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+
         for (int i = 2; i < coordinates.length; i++) {
-            int currentDeltaX = coordinates[i][0] - coordinates[0][0];
-            int currentDeltaY = coordinates[i][1] - coordinates[0][1];
-            
-            if (deltaY * currentDeltaX != deltaX * currentDeltaY) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+
+            if (dy * (x - x0) != dx * (y - y0)) {
                 return false;
             }
         }
-        
         return true;
     }
 }
